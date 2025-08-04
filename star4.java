442404c773790941164a4ef65c9553fc653d253f
Sudarshan Ingole <149046829+Sudarshan7276@users.noreply.github.com>
2025-08-04 14:07:11 +0530

    Add files via upload

---
+++ star4.java
@@ -0,0 +1,10 @@
+public class star4{
+public static void main(String[]args){
+int i=5;
+for( i=5;i>=1;i--){
+for(int j=1;j<=i;j++){
+for (int k=1;k<=j;k++){
+System.out.print("*");}
+System.out.print("$");}
+System.out.println();
+}}}
